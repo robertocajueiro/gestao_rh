85e399657a738d9485275a8124208510f834eb91
Roberto Dantas <beto.cajueiro@gmail.com>
2020-05-03 22:02:30 -0300

    Criando app funcionarios

--- apps/empresas/models.py
+++ apps/empresas/models.py
@@ -2,4 +2,4 @@ from django.db import models
 
 
 class Empresa(models.Model):
-    nome = models.CharField(max_length=100, help_text='Nome da empresa')
+    nome = models.CharField(max_length=100, help_text='Nome da Empresa')
