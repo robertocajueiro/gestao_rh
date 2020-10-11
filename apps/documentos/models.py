8a96eda222101cb41f6f5ca1e1535fe3c3bbdff6
robertocajueiro <beto.cajueiro@gmail.com>
2020-10-11 12:36:45 -0300

    Add duas linhas
     para teste do git Desktop

--- apps/documentos/models.py
+++ apps/documentos/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 from apps.funcionarios.models import Funcionario
 
+
 class Documento(models.Model):
     descricao = models.CharField(max_length=100)
     pertence = models.ForeignKey(Funcionario, on_delete=models.PROTECT)
